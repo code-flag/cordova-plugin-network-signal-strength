50b55dabefc97ccfc65ee22fd92612b53d910094
code-flag <awefrancolaz@gmail.com>
2022-08-13 04:47:29 +0100

    chore: network type and network connectionstate added

--- src/android/NetworkSignalStrength.java
+++ src/android/NetworkSignalStrength.java
@@ -3,90 +3,218 @@
  */
 package org.apache.cordova.networksignalstrength;
 
-import java.util.ArrayList;
-
 import android.content.Context;
-
+import android.net.ConnectivityManager;
 import android.telephony.TelephonyManager;
+import android.net.NetworkInfo;
+import java.util.ArrayList;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
 // import android.telephony.TelephonyCallback;
 
 import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.CordovaInterface;
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class NetworkSignalStrength extends CordovaPlugin {
 
-    private static final int PERMISSION_REQUEST_CODE = 100;
-    TelephonyManager telephonyManager;
-    
+  private static final int PERMISSION_REQUEST_CODE = 100;
+  TelephonyManager telephonyManager;
+  TelephonyManager tmanager = (TelephonyManager) cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
+  ConnectivityManager cmanager = (ConnectivityManager) cordova.getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("netSignalStrength")) {
-            // String message = args.getString(0);
-            // this.coolMethod(message, callbackContext);
-            // create object of class SignalStrengthStateListener 
-            // networkSignalListener = new SignalStrengthStateListener();
-                TelephonyManager tm = (TelephonyManager) cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
-                /**
-                    Callback invoked when network signal strengths changes on the registered subscription. Note, 
-                    the registration subscription ID comes from TelephonyManager object which registers 
-                    TelephonyCallback by TelephonyManager#registerTelephonyCallback(Executor, TelephonyCallback).
-                 */
-                // tm.listen(networkSignalListener, TelephonyCallback.SignalStrengthsListener );
-                // int counter = 0;
-                // while ( dbm == -1) {
-                //         try {
-                //                 Thread.sleep(200);
-                //         } catch (InterruptedException e) {
-                //                 e.printStackTrace();
-                //         }
-                //         if (counter++ >= 5)
-                //         {
-                //                 break;
-                //         }
-                // }
-                // Log.d("signal details", tm.getSignalStrength());
-                // Log.d("signal dbm", dbm);
-                callbackContext.success(tm.getSignalStrength().toString());
-            return true;
+//   TelephonyManager tmanager = (TelephonyManager) _context.getSystemService(
+//       Context.TELEPHONY_SERVICE
+//     );
+
+    
+  @Override
+  public boolean execute(
+    String action,
+    JSONArray args,
+    CallbackContext callbackContext
+  )
+    throws JSONException {
+
+    if (action.equals("signalStrength")) {
+      callbackContext.success(getSignalStrength());
+      return true;
+    }
+    if (action.equals("networkType")) {
+        if(connectionStatus()) {
+            callbackContext.success(String.valueOf(getCurrentNetworkType()));
+        }else {
+            callbackContext.success(String.valueOf(0));
         }
-        return false;
+      return true;
+    }
+    if (action.equals("connectionStatus")) {
+      callbackContext.success(String.valueOf(connectionStatus()));
+      return true;
+    }
+    if (action.equals("networkDetails")) {
+        if(connectionStatus()) {
+            String networkType = String.valueOf(getCurrentNetworkType());
+            String networkDetail = "1^" + networkType + "^" + getSignalStrength();
+            callbackContext.success(networkDetail);
+        }else {
+            callbackContext.success(String.valueOf(0));
+        }
+      return true;
+    }
+    return false;
+  }
+
+/**
+ * initial test method for signal strength
+ *
+ */
+
+ public String getSignalStrength() {
+    return tmanager.getSignalStrength().toString();
+ }
+ 
+  /**
+   * Check if the device is connected to the internet (mobile network or
+   * WIFI).
+   */
+  public boolean connectionStatus() {
+    boolean online = false;
+    if (tmanager != null) {
+      if (tmanager.getDataState() == TelephonyManager.DATA_CONNECTED) {
+        // Mobile network
+        online = true;
+      } else {
+        // WIFI
+        
+        if (cmanager != null) {
+          NetworkInfo info = cmanager.getActiveNetworkInfo();
+          if (info != null) online = info.isConnected();
+        }
+      }
     }
 
-    // private void coolMethod(String message, CallbackContext callbackContext) {
-    //     if (message != null && message.length() > 0) {
-    //         callbackContext.success(message);
-    //     } else {
-    //         callbackContext.error("Expected one non-empty string argument.");
-    //     }
-    // }
+    return online;
+  }
+
+  /**
+   * Evaluate the current network connection and return the
+   * corresponding type, e.g. CONNECTION_WIFI.
+   */
+  public byte getCurrentNetworkType() {
+
+    // connection types
+    byte CONNECTION_UNKNOWN = -1; 
+    byte CONNECTION_OFFLINE = 0; 
+    byte CONNECTION_ROAMING = 5;
+    byte CONNECTION_WIFI = 1; 
+    byte CONNECTION_2G = 2; 
+    byte CONNECTION_3G = 3;
+    byte CONNECTION_4G = 4; 
+
+    NetworkInfo netInfo = cmanager.getActiveNetworkInfo();
+
+    if (netInfo == null) return CONNECTION_OFFLINE;
+
+    if (
+      netInfo.getType() == ConnectivityManager.TYPE_WIFI
+    ) return CONNECTION_WIFI;
+
+    if (netInfo.isRoaming()) return CONNECTION_ROAMING;
+
+    if (
+      netInfo.getType() == ConnectivityManager.TYPE_MOBILE
+    ) { // NETWORK_TYPE_HSPAP
+      switch (netInfo.getSubtype()) {
+        case TelephonyManager.NETWORK_TYPE_GPRS:
+        case TelephonyManager.NETWORK_TYPE_EDGE:
+        case TelephonyManager.NETWORK_TYPE_CDMA:
+        case TelephonyManager.NETWORK_TYPE_1xRTT:
+        case TelephonyManager.NETWORK_TYPE_IDEN:
+          return CONNECTION_2G;
+        case TelephonyManager.NETWORK_TYPE_UMTS:
+        case TelephonyManager.NETWORK_TYPE_EVDO_0:
+        case TelephonyManager.NETWORK_TYPE_EVDO_A:
+        case TelephonyManager.NETWORK_TYPE_HSDPA:
+        case TelephonyManager.NETWORK_TYPE_HSUPA:
+        case TelephonyManager.NETWORK_TYPE_HSPA:
+        case TelephonyManager.NETWORK_TYPE_EVDO_B:
+        case TelephonyManager.NETWORK_TYPE_EHRPD:
+        case TelephonyManager.NETWORK_TYPE_HSPAP:
+          return CONNECTION_3G;
+        case TelephonyManager.NETWORK_TYPE_LTE:
+          return CONNECTION_4G;
+        default:
+          return CONNECTION_UNKNOWN;
+      }
+    }
+
+    return CONNECTION_OFFLINE;
+  }
+
+    /**
+     * initial test method for signal strength
+     *
+     */
+    //  public static netSignalstrength(Context Context){
+    //     // String message = args.getString(0);
+    //   // this.coolMethod(message, callbackContext);
+    //   // create object of class SignalStrengthStateListener
+    //   // networkSignalListener = new SignalStrengthStateListener();
+    //   TelephonyManager tm = (TelephonyManager) cordova
+    //     .getActivity()
+    //     .getSystemService(Context.TELEPHONY_SERVICE);
+    //   /**
+    //                 Callback invoked when network signal strengths changes on the registered subscription. Note, 
+    //                 the registration subscription ID comes from TelephonyManager object which registers 
+    //                 TelephonyCallback by TelephonyManager#registerTelephonyCallback(Executor, TelephonyCallback).
+    //              */
+    //   // tm.listen(networkSignalListener, TelephonyCallback.SignalStrengthsListener );
+    //   // int counter = 0;
+    //   // while ( dbm == -1) {
+    //   //         try {
+    //   //                 Thread.sleep(200);
+    //   //         } catch (InterruptedException e) {
+    //   //                 e.printStackTrace();
+    //   //         }
+    //   //         if (counter++ >= 5)
+    //   //         {
+    //   //                 break;
+    //   //         }
+    //   // }
+    //   // Log.d("signal details", tm.getSignalStrength());
+    //   // Log.d("signal dbm", dbm);
+    //  }
+    
+  // private void coolMethod(String message, CallbackContext callbackContext) {
+  //     if (message != null && message.length() > 0) {
+  //         callbackContext.success(message);
+  //     } else {
+  //         callbackContext.error("Expected one non-empty string argument.");
+  //     }
+  // }
+
+  // class SignalStrengthStateListener extends TelephonyCallback {
+
+  // @Override
+  // public void onSignalStrengthsChanged(SignalStrength signalStrength) {
+  //         super.onSignalStrengthsChanged(signalStrength);
+  //         int tsNormSignalStrength = signalStrength.getDbm();
+  //         dbm = tsNormSignalStrength;     // -> dBm
+  //         // Log.d("signal details tsNormSignalStrength", tsNormSignalStrength);
+
+  // }
+
+  // }
+
+  // create object of class SignalStrengthStateListener
+  // SignalStrengthStateListener networkSignalListener;
+  // int dbm = -1;
 
 
-// class SignalStrengthStateListener extends TelephonyCallback {
-
-// @Override
-// public void onSignalStrengthsChanged(SignalStrength signalStrength) {
-//         super.onSignalStrengthsChanged(signalStrength);
-//         int tsNormSignalStrength = signalStrength.getDbm();
-//         dbm = tsNormSignalStrength;     // -> dBm
-//         // Log.d("signal details tsNormSignalStrength", tsNormSignalStrength);
-        
-// }
-
-
-// }
-
-// create object of class SignalStrengthStateListener 
-    // SignalStrengthStateListener networkSignalListener;
-    // int dbm = -1;
 }
--- www/NetworkSignalStrength.js
+++ www/NetworkSignalStrength.js
@@ -1,16 +1,98 @@
 var exec = require('cordova/exec');
-/*
-networkSignalStrength = function (arg0, success, error) {
-    exec(success, error, 'NetworkSignalStrength', 'netSignalStrength', [arg0]);
-};
-*/
+
+var mainCallbackFunction = null;
+/**
+ * @var {object} signalTypeRange - defines signal strength properties with range
+ */
+const signalTypeRange = {
+    RSSI: [-113,-51, 'dBm'],
+    RSSNR: [-20, +30, 'dB'],
+    RSRQ: [-34, 3, 'dB'],
+    RSRP: [-140,-43, 'dBm']
+   };
+
+/**
+ * @var {object} signalType - defines signal strength properties
+ */
+const signalType = ["RSSI", "RSSNR", "RSRQ", "RSRP", "LEVEL", "LTELEVEL"];
+
+/**
+ * This method extract signal strength needed information from the return value
+ * such as rssi rsrq rsnnr level lte level - all in dBm unit
+ * @param {string} signals - signal strength returned value
+ * @returns object
+ */
+const getSignalList = (signals) => {
+    var netSignal = [] ;
+    let newDataObj;
+    signals.split(':')[2].split(' ').filter((data) => {
+      if (data !== "") {
+        newDataObj = data.split('=');
+        if (signalType.includes(newDataObj[0]?.toUpperCase())) {
+          newDataObj = {[newDataObj[0]]: newDataObj[1]};
+          netSignal.push(newDataObj);
+        }
+      } 
+    });
+    return netSignal;
+  }
+
+  /**
+   * This function gets the return value, format it to a meaningful object data and 
+   * pass it to a callback function
+   * @param {string} networkDetails - returned value
+   * @param {function} callback - callback function
+   */
+const returnNetworkDetails = (networkDetails) => {
+    let incomingDataArr = networkDetails.split('^');
+    let connectionState = incomingDataArr[0] == 1 ? 'CONNECTED' : 'DISCONNECTED';
+    let networkType = incomingDataArr[1];
+    let signalStrength = getSignalList(incomingDataArr[2]);
+    const networkDetailsObj = {
+        CONNECTION_STATE : connectionState,
+        SIGNAL_STRENGTH : networkType,
+        NETWORK_TYPE : signalStrength 
+    }
+
+    mainCallbackFunction(networkDetailsObj);
+}
+
+/**
+ * This function gets extracted signal strength details and 
+ * invoke callback function
+ * @param {string} data - incoming data or return data
+ * @param {function} callback - callback function to invoke
+ */
+const returnSignalList = (data) => {
+    mainCallbackFunction(getSignalList(data));
+}
 
 function SignalStrength() {
-    this.networkSignalStrength = function (callback) {
-        return cordova.exec(callback, function(err) {
+    this.networkInfo = function (infoType, callback) {
+        if (infoType === 'CONNECTION_STATE') {
+            return cordova.exec(callback, function(err) {
             callback(-1);
-          }, 'NetworkSignalStrength', 'netSignalStrength', []);
+          }, 'NetworkSignalStrength', 'connectionStatus', []);
+        }
+        else if (infoType === 'NETWORK_TYPE') {
+            return cordova.exec(callback, function(err) {
+            callback(-1);
+          }, 'NetworkSignalStrength', 'networkType', []);
+        }
+        else if (infoType === 'SIGNAL_STRENGTH') {
+            mainCallbackFunction = callback;
+            return cordova.exec(returnSignalList, function(err) {
+            callback(-1);
+          }, 'NetworkSignalStrength', 'signalStrength', []);
+        }
+        else {
+            mainCallbackFunction = callback;
+            return cordova.exec(returnNetworkDetails, function(err) {
+            callback(-1);
+          }, 'NetworkSignalStrength', 'networkDetails', []);
+        }
+        
     };
   }
   
-  window.netSignalStrength = new SignalStrength()
+  window.networkSignalStrength = new SignalStrength()
